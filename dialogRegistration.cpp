3bb729734f626d5c756293c3a64873206e1efa7c
KonstantinBarni <konstantin.barni@gmail.com>
2024-07-17 12:19:31 +0500

    start project

---
+++ dialogRegistration.cpp
@@ -0,0 +1,14 @@
+#include "dialogRegistration.h"
+#include "ui_dialogRegistration.h"
+
+DialogRegistration::DialogRegistration(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::DialogRegistration)
+{
+    ui->setupUi(this);
+}
+
+DialogRegistration::~DialogRegistration()
+{
+    delete ui;
+}
---
+++ mainwindow.cpp
@@ -0,0 +1,46 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    qDebug() << QSqlDatabase::drivers();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_actionOpen_chat_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionClose_this_chat_triggered()
+{
+
+}
+
+
+void MainWindow::on_all_chatButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_registrationButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_joinButton_clicked()
+{
+
+}
+
